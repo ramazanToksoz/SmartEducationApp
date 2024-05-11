9f0e91e7c7234cd40e8f0c66b2b85b79e710bedd
ramazanToksoz <rtoksoz7@gmail.com>
2024-05-11 15:24:54 +0300

    lesson 02

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app=express()
-const PORT=3000
+
+// tepmlete engine
+app.set("view engine","ejs")
+
+//middleware
+app.use(express.static("public"))
 
 
 app.get("/",(req,res )=> {
-  res.status(200).send("index sayfası")
+  res.status(200).render("index",{
+    page_name:"index"
+  })
+})
+app.get("/about",(req,res )=> {
+  res.status(200).render("about",{
+    page_name:"about"
+  })
 })
 
 
 
 
-
+const PORT=3000
 app.listen(PORT,(req,res) => {
   console.log(`port listening ${PORT}`);
 })
